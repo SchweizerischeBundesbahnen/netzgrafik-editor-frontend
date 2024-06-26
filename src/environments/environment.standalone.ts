ec90c4865f162684b0efc0c72655b5c476c698a8
Simon Ser <contact@emersion.fr>
2024-06-26 17:29:30 +0200

    Switch production flag on for standalone build

--- src/environments/environment.standalone.ts
+++ src/environments/environment.standalone.ts
@@ -1,7 +1,7 @@
 import {Environment} from "./environment.model";
 
 export const environment: Environment = {
-  production: false,
+  production: true,
   label: "standalone",
   disableBackend: true,
   customElement: true,
