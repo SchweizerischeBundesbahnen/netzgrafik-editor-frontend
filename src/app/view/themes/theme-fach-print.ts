e6b519ed4ecfa2c22ed608078a33441cbe2dda4a
adrian_egli <adrian.egli@gmail.com>
2024-07-17 09:41:21 +0200

    bug fixed (npm run test failed)

--- src/app/view/themes/theme-fach-print.ts
+++ src/app/view/themes/theme-fach-print.ts
@@ -5,7 +5,7 @@ import {StaticDomTags} from "../editor-main-view/data-views/static.dom.tags";
 
 export class ThemeFachPrint extends ThemeBase {
   constructor(
-    backgroundColor = "white",
+    backgroundColor = "whitesmoke",
     backgroundStreckengraphikColor = "white",
   ) {
     super(
