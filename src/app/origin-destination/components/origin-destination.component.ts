e6edbdb2f4b0b5f1dfeff136934295b4b7b83a6a
Uriel-Sautron <uriel.sautron@gmail.com>
2025-01-14 15:12:45 +0100

    feat: origin destination
      - create folder and files for display matrices OD

---
+++ src/app/origin-destination/components/origin-destination.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "sbb-origin-destination",
+  templateUrl: "./origin-destination.component.html",
+  styleUrls: ["./origin-destination.component.scss"],
+})
+export class OriginDestinationComponent {
+  constructor() {}
+}
