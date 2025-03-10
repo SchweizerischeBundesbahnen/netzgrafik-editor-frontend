0660872b9b222ff9c244d8d717b3dec7ae8f3369
Yoh <durandyohan@zaclys.net>
2025-03-10 18:26:23 +0100

    setup: add nix flake (#406)
    * setup: add nix flake
    
    nix package manager users can setup a shell environnement based on a
    flake.nix file that describes tools and versions needed to dev and run
    the project, this flake contains:
    - nodejs 20
    - npm
    - typescript language server
    - angular language service
    
    nix flakes combinated with direnv allow to open a project folder with
    automatic setup launch, creates en .envrc file and a .direnv directory.
    -> not every contributors use it and want to have those dotfiles, so it
    should be ignored by git.
    
    Co-authored-by: Uriel-Sautron <uriel.sautron@gmail.com>
    
    * readme: add nix flake instructions
    
    * fixup! setup: add nix flake
    
    * fixup! readme: add nix flake instructions
    
    ---------
    
    Co-authored-by: Uriel-Sautron <uriel.sautron@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A Nix flake for NGE dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs
+    , flake-utils
+    , ...
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      node = pkgs.nodejs_20;
+      nodePackages = pkgs.nodePackages.override {
+        nodejs = node;
+      };
+      angularLanguageServer = pkgs.angular-language-server;
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          node
+          nodePackages.npm
+          nodePackages.typescript
+          nodePackages.typescript-language-server
+          angularLanguageServer
+        ];
+      };
+    });
+}
