5404dd58a707ea9fd5081e21bf868fc07ff77b06
Adrian Egli <3dhelp@gmail.com>
2025-07-28 23:30:41 +0200

    toggle -> label event click :-)

--- src/app/view/dialogs/trainrun-and-section-dialog/trainrun-roundtrip-tab/trainrun-roundtrip-tab.component.ts
+++ src/app/view/dialogs/trainrun-and-section-dialog/trainrun-roundtrip-tab/trainrun-roundtrip-tab.component.ts
@@ -84,6 +84,10 @@ export class TrainrunRoundtripTabComponent implements OnInit, OnDestroy {
     return retVal + " readonly";
   }
 
+  onToggleOneWay(check){
+    this.onIsOneWayChanged(check);
+  }
+
   onIsOneWayChanged(isChecked: boolean) {
     this.isOneWay = isChecked;
     if (!isChecked && this.selectedTrainrun.getTrainrunDirection() !== TrainrunDirection.ROUND_TRIP) {
